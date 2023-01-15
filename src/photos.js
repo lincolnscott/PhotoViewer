4fdaa7f415ffed3330c2de5b0e24b1e095cf7bca
lincolnscott <lincoln.jae.scott@gmail.com>
2023-01-14 18:04:34 -0600

    initial commit

---
+++ src/photos.js
@@ -0,0 +1,19 @@
+const photos = [
+    { src: "/photos/Hogs/UglyHog1.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog2.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog3.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog4.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog5.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog7.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog8.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog9.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog6.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog10.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog11.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog12.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog13.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog14.png", width: 512, height: 512 },
+    { src: "/photos/Hogs/UglyHog15.png", width: 512, height: 512 },   
+];
+
+export default photos;  
